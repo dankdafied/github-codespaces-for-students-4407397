5ac0f3fc2edab921844fc69b61644a4beef6c97e
Ryan Denman <33558778+dankdafied@users.noreply.github.com>
2024-02-03 15:19:25 +0000

    updated the hammer so I can lay it down

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="hammer">&#128296</span> React
+          GitHub Codespaces <span className="hammer">🔨</span> React
         </p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
